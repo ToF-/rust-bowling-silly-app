26cdf9b8d32a6f474940487e893253c33e1ec3c3
ToF- <cthibauttof@gmail.com>
2026-07-10 15:38:30 +0200

    first dummy test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    
+    use speculoos::assert_that;
+
+    #[test]
+    fn dummy_test() {
+        assert_that!(2+2 == 4);
+    }
 }
