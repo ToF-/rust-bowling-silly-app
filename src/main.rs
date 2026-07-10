b8669dfdcf9c3fa8d79ecc272e2ed64f70d2f86e
ToF- <cthibauttof@gmail.com>
2026-07-11 01:06:13 +0200

    home using web data and a template

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,30 @@ use actix_web::get;
 use actix_web::web;
 use actix_web::web::ServiceConfig;
 
+struct AppState {
+    version: String,
+}
+
 #[derive(Template, WebTemplate)]
 #[template(path = "home.html")]
 pub struct HomePage {
     version: String,
 }
 
-pub async fn home() -> impl Responder {
+pub async fn home(data: web::Data<AppState>) -> impl Responder {
+    let version = &data.version;
     HomePage {
-        version: "1.0.0".to_string(),
+        version: version.to_string(),
     }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().configure(routes))
+    HttpServer::new(|| App::new()
+        .app_data(web::Data::new(AppState {
+            version: "1.0.0".to_string(),
+        }))
+        .configure(routes))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
@@ -39,12 +48,17 @@ mod tests {
     use actix_web::web;
     use crate::home;
     use crate::routes;
+    use crate::AppState;
     use speculoos::assert_that;
     use speculoos::prelude::StrAssertions;
 
     #[actix_web::test]
     async fn test_app_displays_hello_world() {
-        let app = test::init_service(App::new().configure(routes)).await;
+        let app = test::init_service(App::new()
+            .app_data(web::Data::new(AppState {
+                version: "1.0.0".to_string(),
+            }))
+            .configure(routes)).await;
         let request = test::TestRequest::default().to_request();
         let body = test::call_and_read_body(&app, request)
             .await
@@ -54,12 +68,16 @@ mod tests {
     }
     #[actix_web::test]
     async fn test_app_displays_a_version() {
-        let app = test::init_service(App::new().configure(routes)).await;
+        let app = test::init_service(App::new()
+            .app_data(web::Data::new(AppState {
+                version: "1.0.2".to_string(),
+            }))
+            .configure(routes)).await;
         let request = test::TestRequest::default().to_request();
         let body = test::call_and_read_body(&app, request)
             .await
             .escape_ascii()
             .to_string();
-        assert_that(&body).contains("1.0.0");
+        assert_that(&body).contains("1.0.2");
     }
 }
