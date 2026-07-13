14580db5d98c016bc9743ff906ac19aa9aa06dd2
ToF- <cthibauttof@gmail.com>
2026-07-13 09:55:12 +0200

    bowling spare and strike

--- src/bowling.rs
+++ src/bowling.rs
@@ -42,46 +42,99 @@ impl Game {
             self.score += roll
         }
     }
+
+    fn spare(&mut self) -> bool {
+        match self.last_roll {
+            None => false,
+            Some(last) => {
+                self.add_roll(10 - last);
+                true
+            }
+        }
+    }
+
+    fn strike(&mut self) -> bool {
+        match self.last_roll {
+            None => {
+                self.add_roll(10);
+                true
+            }
+            _ => false
+        }
+    }
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use speculoos::assert_that;
+    use speculoos::boolean::BooleanAssertions;
+
 
     #[test]
     fn init_game_puts_score_to_zero() {
         let game = Game::new();
-        speculoos::assert_that!(game.score()).is_equal_to(0);
+        speculoos::assert_that(&game.score()).is_equal_to(0);
     }
     #[test]
     fn add_average_roll_scores_average() {
         let mut game = Game::new();
         game.add_roll(4);
         game.add_roll(3);
-        speculoos::assert_that!(game.score()).is_equal_to(7);
+        speculoos::assert_that(&game.score()).is_equal_to(7);
     }
     #[test]
     fn add_a_spare_scores_a_bonus() {
         let mut game = Game::new();
         game.add_roll(5);
-        game.add_roll(5);
+        game.spare();
         game.add_roll(4);
-        speculoos::assert_that!(game.score()).is_equal_to(18);
+        speculoos::assert_that(&game.score()).is_equal_to(18);
     }
     #[test]
     fn add_a_strike_scores_two_boni() {
         let mut game = Game::new();
-        game.add_roll(10);
+        game.strike();
         game.add_roll(5);
         game.add_roll(4);
-        speculoos::assert_that!(game.score()).is_equal_to(28);
+        speculoos::assert_that(&game.score()).is_equal_to(28);
     }
     #[test]
     fn twelve_strikes_scores_a_perfect() {
         let mut game = Game::new();
-        for i in 0..12 {
-            game.add_roll(10)
+        for _ in 0..12 {
+            game.strike();
         };
-        speculoos::assert_that!(game.score()).is_equal_to(300);
+        speculoos::assert_that(&game.score()).is_equal_to(300);
+    }
+    #[test]
+    fn complete_a_spare_change_the_score() {
+        let mut game = Game::new();
+        game.add_roll(4);
+        speculoos::assert_that(&game.spare());
+        game.add_roll(5);
+        speculoos::assert_that(&game.score()).is_equal_to(20);
+    }
+    #[test]
+    fn complete_a_spare_cant_change_the_score_on_new_frame() {
+        let mut game = Game::new();
+        game.add_roll(4);
+        game.add_roll(4);
+        speculoos::assert_that(&game.spare()).is_false();
+        speculoos::assert_that(&game.score()).is_equal_to(8);
+    }
+    #[test]
+    fn strike_change_the_score() {
+        let mut game = Game::new();
+        speculoos::assert_that(&game.strike());
+        game.add_roll(4);
+        game.add_roll(3);
+        speculoos::assert_that(&game.score()).is_equal_to(24);
+    }
+    #[test]
+    fn strike_cant_change_the_score_on_half_frame() {
+        let mut game = Game::new();
+        game.add_roll(4);
+        speculoos::assert_that(&game.strike()).is_false();
     }
 }
